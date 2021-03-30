3c3b50a6f8b01297366fe4182b170272329f9151
ruoshui-git <ruoshuim@gmail.com>
2021-03-30 10:39:58 -0400

    feat: add file hashing

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,12 @@ import livereload from "rollup-plugin-livereload";
 import { terser } from "rollup-plugin-terser";
 import sveltePreprocess from "svelte-preprocess";
 import typescript from "@rollup/plugin-typescript";
-import css from "rollup-plugin-css-only";
+// import css from "rollup-plugin-css-only";
+
+import postcss from "rollup-plugin-postcss";
+import html from "@rollup/plugin-html";
+import { promises as fs } from "fs";
+import path from "path";
 import json from "@rollup/plugin-json";
 
 const production = !process.env.ROLLUP_WATCH;
@@ -22,7 +27,7 @@ function serve() {
             if (server) return;
             server = require("child_process").spawn(
                 "npm",
-                ["run", "start", "--", "--dev"],
+                ["run", "start-dev", "--", "--dev"],
                 {
                     stdio: ["ignore", "inherit", "inherit"],
                     shell: true,
@@ -35,28 +40,119 @@ function serve() {
     };
 }
 
+function copyPublic(publicDir) {
+    return {
+        name: "copy-public",
+        async generateBundle({ dir }) {
+            if (!(await fs.stat(publicDir)).isDirectory()) {
+                throw Error(`Provided path should be a dir`);
+            }
+            const srcDir = await fs.readdir(publicDir);
+            await fs.access(dir).catch((e) => fs.mkdir(dir));
+            for (const file of srcDir) {
+                if (file !== "index.html" && file !== "build") {
+                    await fs.copyFile(
+                        path.format({
+                            dir: publicDir,
+                            base: file,
+                        }),
+                        path.format({
+                            dir,
+                            base: file,
+                        })
+                    );
+                }
+            }
+        },
+    };
+}
+
+const htmlOptions = {
+    template: async ({ attributes, files, meta, publicPath, title }) => {
+        let htmlTmpl = await fs.readFile("public/index.html", {
+            encoding: "utf-8",
+        });
+
+        if (files.css) {
+            htmlTmpl = htmlTmpl.replace(
+                "build/bundle.css",
+                files.css[0].fileName
+            );
+        }
+        if (files.js) {
+            htmlTmpl = htmlTmpl.replace(
+                "build/bundle.js",
+                files.js[0].fileName
+            );
+        }
+        return htmlTmpl;
+    },
+};
+
 export default {
     input: "src/main.ts",
-    output: {
-        sourcemap: true,
-        // format: "iife",
-        format: "iife",
-        name: "app",
-        file: "public/build/bundle.js",
-    },
+    output: production
+        ? {
+              format: "iife",
+              sourcemap: false,
+              entryFileNames: "[name].[hash].js",
+              chunkFileNames: "[name].[hash].js",
+              name: "app",
+              // file: "public/build/bundle.js",
+              dir: "build/",
+          }
+        : {
+              sourcemap: true,
+              format: "iife",
+              name: "app",
+              file: "public/build/bundle.js",
+          },
     plugins: [
         json(),
+        production && copyPublic("public"),
         svelte({
             preprocess: sveltePreprocess({ sourceMap: !production }),
+            emitCss: true,
             compilerOptions: {
                 // enable run-time checks when not in production
                 dev: !production,
             },
         }),
-        // we'll extract any component CSS out into
-        // a separate file - better for performance
-        css({ output: "bundle.css" }),
-
+        // // we'll extract any component CSS out into
+        // // a separate file - better for performance
+        // // Default behaviour is to write all styles to the bundle destination where .js is replaced by .css
+        // css({
+        //     output: function (styles, _styleNodes, bundle) {
+        //         // console.log(bundle);
+        //         for (const file of Object.values(bundle)) {
+        //             if (file.isEntry) {
+        //                 let dest = file.fileName;
+        //                 if (dest.endsWith(".js")) {
+        //                     dest = dest.slice(0, -3);
+        //                 }
+        //                 dest = dest + ".css";
+        //                 console.log(this);
+        //                 writeFileSync(
+        //                     path.join(path.resolve("."), outDir, dest),
+        //                     styles
+        //                 );
+        //                 // this.emitFile({ type: 'asset', fileName: dest, source: css });
+        //             }
+        //         }
+        //     },
+        // }),
+        postcss({
+            extract: true,
+            minimize: true,
+            use: [
+                [
+                    "sass",
+                    {
+                        includePaths: ["./src/theme", "./node_modules"],
+                    },
+                ],
+            ],
+        }),
         // If you have external dependencies installed from
         // npm, you'll most likely need these plugins. In
         // some cases you'll need additional configuration -
@@ -83,6 +179,7 @@ export default {
         // If we're building for production (npm run build
         // instead of npm run dev), minify
         production && terser(),
+        production && html(htmlOptions),
     ],
     watch: {
         clearScreen: false,
